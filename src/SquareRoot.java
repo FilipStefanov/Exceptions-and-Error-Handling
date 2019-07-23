3e76a485745806a38ecb1dbc512c50d6a3dc3418
Filip Stefanov <f.stefanov@yahoo.com>
2019-07-23 20:05:06 +0300

    second

--- src/SquareRoot.java
+++ src/SquareRoot.java
@@ -4,13 +4,13 @@ public class SquareRoot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-      try {
-          int n = Integer.parseInt(scanner.nextLine());
-          System.out.println(Math.sqrt(n));
-      }catch (IllegalArgumentException e){
-          System.out.println("Invalid number");
-      }finally {
-          System.out.println("Good bye");
-      }
+        try {
+            int n = Integer.parseInt(scanner.nextLine());
+            System.out.println(Math.sqrt(n));
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid number");
+        } finally {
+            System.out.println("Good bye");
+        }
     }
 }
